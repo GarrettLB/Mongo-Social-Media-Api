47445dd70884e262940abab4d6eb0052da3fd23a
GLBryan <garrettleebryan@gmail.com>
2022-01-03 11:34:03 -0500

    deleteUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,4 +30,15 @@ module.exports = {
       .then (user => res.status(200).json(user))
       .catch((err) => res.status(500).json(err));
   },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId }, (err, result) => {
+        if (result) {
+          res.status(200).json(result);
+          console.log(`Deleted: ${result}`);
+        } else {
+          console.log('Uh Oh, something went wrong');
+          res.status(500).json({ message: 'something went wrong' });
+        }
+      });
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,12 +5,15 @@ const {
   getSingleUser,
   createUser,
   updateUser,
+  deleteUser,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser);
 
-router.route('/userId/:newUsername').put(updateUser)
+router.route('/userId/:newUsername').put(updateUser);
+
+router.route('/userId').delete(deleteUser);
 
 module.exports = router;
