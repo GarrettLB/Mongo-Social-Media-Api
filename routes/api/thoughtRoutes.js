37f90bb5930bdcdddb4ddec2d243f9448feb6d76
GLBryan <garrettleebryan@gmail.com>
2022-01-03 11:05:41 -0500

    thoughtController thoughtRoute

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+} = require('../../controllers/thoughtController');
 
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:userId').get(getSingleThought);
 
 module.exports = router;
