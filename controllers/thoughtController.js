a38ddde2831304b6a939571f1e898c17151d8818
GLBryan <garrettleebryan@gmail.com>
2022-01-07 11:58:05 -0500

    better thoughtcontroller delete

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,5 @@
 const Thought = require('../models/Thought');
+const User = require('../models/User');
 
 module.exports = {
   getThoughts(req, res) {
@@ -35,14 +36,23 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
   deleteThought(req, res) {
-    Thought.findOneAndDelete({ _id: req.params.thoughtId }, (err, result) => {
-        if (result) {
-          res.status(200).json(result);
-          console.log(`Deleted: ${result}`);
-        } else {
-          console.log('Uh Oh, something went wrong');
-          res.status(500).json({ message: 'something went wrong' });
-        }
-      });
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with this id!' })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'Thought deleted but no user with this id!' })
+          : res.json({ message: 'Thought successfully deleted!' })
+      )
+      .catch((err) => res.status(500).json(err));
   },
 };
